e2fb4edfde1beac8172f20a1145c6ca32a711575
LuisHans <jluishans@outlook.com>
2021-09-13 17:52:49 -0500

    Adding a Cart Context Correction

--- src/store/card-context.js
+++ src/store/card-context.js
@@ -3,9 +3,8 @@ import React from 'react';
 const CardContext =  React.createContext({
     items: [],
     totalAmount: 0,
-    addItem: () => {
-        removeItem: (id) => {}
-    }
+    addItem: () => {},
+    removeItem: (id) => {}
 });
 
 export default CardContext;
